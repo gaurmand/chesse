e66b4ed5a6425f4f192de1483bd314b508c066ed
Gaurav Amwan <gvamwan@gmail.com>
2022-11-05 15:14:54 -0700

    Add board equality operator

--- chess/board.cpp
+++ chess/board.cpp
@@ -3,23 +3,27 @@
 namespace Chess
 {
 
+//=============================================================================
 Board::Board()
 {
    setDefault();
 }
 
+//=============================================================================
 void Board::setEmpty(Square sqr)
 {
    board_[sqr] = Piece::Empty;
    colour_[sqr] = Colour::Empty;
 }
 
+//=============================================================================
 void Board::setPiece(Square sqr, Piece piece, Colour color)
 {
    board_[sqr] = piece;
    colour_[sqr] = color;
 }
 
+//=============================================================================
 void Board::move(Square from, Square to)
 {
    board_[to] = board_[from];
@@ -27,12 +31,14 @@ void Board::move(Square from, Square to)
    setEmpty(from);
 }
 
+//=============================================================================
 void Board::setEmpty()
 {
    board_.fill(Piece::Empty);
    colour_.fill(Colour::Empty);
 }
 
+//=============================================================================
 void Board::setDefault()
 {
    board_ = {
@@ -56,6 +62,12 @@ void Board::setDefault()
       Colour::Black, Colour::Black, Colour::Black, Colour::Black, Colour::Black, Colour::Black, Colour::Black, Colour::Black,
       Colour::Black, Colour::Black, Colour::Black, Colour::Black, Colour::Black, Colour::Black, Colour::Black, Colour::Black
    };
+}
+
+//=============================================================================
+bool Board::operator==(const Board& rhs) const
+{
+   return board_ == rhs.board_ && colour_ == rhs.colour_;
 }
 
 }  // namespace Chess
