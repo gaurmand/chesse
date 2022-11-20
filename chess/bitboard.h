af8ea15e4bedce33661be8df85d06825492e77f4
Gaurav Amwan <gvamwan@gmail.com>
2022-11-20 14:21:15 -0800

    Make constexpr Bitboard methods

--- chess/bitboard.h
+++ chess/bitboard.h
@@ -4,6 +4,7 @@
 #include "square.h"
 
 #include <cstdint>
+#include <array>
 
 namespace Chess
 {
@@ -11,44 +12,56 @@ namespace Chess
 constexpr uint64_t kEmptyBoard = 0;
 constexpr uint64_t kFullBoard  = 0xFFFFFFFFFFFFFFFFull;
 
+constexpr std::array<uint64_t, NUM_SQUARES> squareMask = {
+   1ull << 0,  1ull << 1,  1ull << 2,  1ull << 3,  1ull << 4,  1ull << 5,  1ull << 6,  1ull << 7,
+   1ull << 8,  1ull << 9,  1ull << 10, 1ull << 11, 1ull << 12, 1ull << 13, 1ull << 14, 1ull << 15, 
+   1ull << 16, 1ull << 17, 1ull << 18, 1ull << 19, 1ull << 20, 1ull << 21, 1ull << 22, 1ull << 23, 
+   1ull << 24, 1ull << 25, 1ull << 26, 1ull << 27, 1ull << 28, 1ull << 29, 1ull << 30, 1ull << 31, 
+   1ull << 32, 1ull << 33, 1ull << 34, 1ull << 35, 1ull << 36, 1ull << 37, 1ull << 38, 1ull << 39, 
+   1ull << 40, 1ull << 41, 1ull << 42, 1ull << 43, 1ull << 44, 1ull << 45, 1ull << 46, 1ull << 47, 
+   1ull << 48, 1ull << 49, 1ull << 50, 1ull << 51, 1ull << 52, 1ull << 53, 1ull << 54, 1ull << 55, 
+   1ull << 56, 1ull << 57, 1ull << 58, 1ull << 59, 1ull << 60, 1ull << 61, 1ull << 62, 1ull << 63
+};
+
 class Bitboard
 {
 public:
    //==========================================================================
-   Bitboard(uint64_t b): board_(b) {}
+   constexpr Bitboard(): board_(kEmptyBoard) {}
+   constexpr Bitboard(uint64_t b): board_(b) {}
 
    //==========================================================================
-   uint64_t to_uint() const { return board_; }
+   constexpr uint64_t to_uint() const { return board_; }
 
    //==========================================================================
-   bool operator==(const Bitboard& r) const { return board_ == r.board_; }
-   bool operator!=(const Bitboard& r) const { return board_ != r.board_; }
+   constexpr bool operator==(const Bitboard& r) const { return board_ == r.board_; }
+   constexpr bool operator!=(const Bitboard& r) const { return board_ != r.board_; }
 
    //==========================================================================
-   Bitboard operator|(const Bitboard& rhs) const { return board_ | rhs.board_; }
-   Bitboard operator&(const Bitboard& rhs) const { return board_ & rhs.board_; }
-   Bitboard operator^(const Bitboard& rhs) const { return board_ ^ rhs.board_; }
-   Bitboard operator~() const { return ~board_; }
+   constexpr Bitboard operator|(const Bitboard& rhs) const { return board_ | rhs.board_; }
+   constexpr Bitboard operator&(const Bitboard& rhs) const { return board_ & rhs.board_; }
+   constexpr Bitboard operator^(const Bitboard& rhs) const { return board_ ^ rhs.board_; }
+   constexpr Bitboard operator~() const { return ~board_; }
 
    //==========================================================================
-   Bitboard& operator|=(const Bitboard& rhs) { board_ |= rhs.board_; return *this; }
-   Bitboard& operator&=(const Bitboard& rhs) { board_ &= rhs.board_; return *this; }
-   Bitboard& operator^=(const Bitboard& rhs) { board_ ^= rhs.board_; return *this; }
-   Bitboard& flip() { board_ = ~board_; return *this; }
+   constexpr Bitboard& operator|=(const Bitboard& rhs) { board_ |= rhs.board_; return *this; }
+   constexpr Bitboard& operator&=(const Bitboard& rhs) { board_ &= rhs.board_; return *this; }
+   constexpr Bitboard& operator^=(const Bitboard& rhs) { board_ ^= rhs.board_; return *this; }
+   constexpr Bitboard& flip() { board_ = ~board_; return *this; }
 
    //==========================================================================
-   bool test(Square sq) const;
+   constexpr bool test(Square sq) const { return board_ & squareMask[sq]; }
    std::size_t count() const;
 
    //==========================================================================
-   bool any() const { return board_ != kEmptyBoard; }
-   bool none() const { return board_ == kEmptyBoard; }
-   bool all() const { return board_ == kFullBoard; }
+   constexpr bool any() const { return board_ != kEmptyBoard; }
+   constexpr bool none() const { return board_ == kEmptyBoard; }
+   constexpr bool all() const { return board_ == kFullBoard; }
 
    //==========================================================================
-   void set(Square sq);
-   void reset(Square sq);
-   void toggle(Square sq);
+   constexpr void set(Square sq) { board_ |= squareMask[sq]; }
+   constexpr void reset(Square sq) { board_ &= ~squareMask[sq]; }
+   constexpr void toggle(Square sq) { board_ ^= squareMask[sq]; }
 
    //==========================================================================
    class Iterator
